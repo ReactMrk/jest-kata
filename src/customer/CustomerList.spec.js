9477fe833f518a51acaa5b17b75a224da7f8e8b8
Jordi <jordi.pastor@sky.uk>
2024-11-15 09:34:59 +0100

    Jordi Pastor update kata 4

--- src/customer/CustomerList.spec.js
+++ src/customer/CustomerList.spec.js
@@ -13,21 +13,20 @@ jest.mock('react', () => ({
 
 jest.mock('./hooks/useAlert', () => jest.fn());
 
+const customers = [
+  {
+    name: 'Bob',
+    email: 'bob@sky.uk',
+    phone: '77777777777',
+    address: 'Watermark'
+  }
+]
 const setHomeAlertText = jest.fn();
 const setHomeAlertVisible = jest.fn();
 const renderComponent = () => render(<CustomerList setHomeAlertText={setHomeAlertText} setHomeAlertVisible={setHomeAlertVisible}/>);
 describe('CustomerList', () => {
   beforeEach(() => {
-    useContext.mockReturnValue({
-      customers: [
-        {
-          name: 'Bob',
-          email: 'bob@sky.uk',
-          phone: '77777777777',
-          address: 'Watermark'
-        }
-      ]
-    });
+    useContext.mockReturnValue({ customers });
     useAlert.mockReturnValue(true);
   });
 
@@ -35,14 +34,14 @@ describe('CustomerList', () => {
     jest.clearAllMocks();
   });
   describe('Client added alert', () => {
-    it('should render an alert with text Welcome to the Client List when displayAlert from hook is true', () => {
+    it('should render an alert with text New client has been added when displayAlert from hook is true', () => {
       renderComponent();
       expect(useContext).toHaveBeenCalledWith(CustomerContext);
       expect(useAlert).toHaveBeenCalled();
       expect(screen.getByText('New client has been added')).toBeInTheDocument();
     });
 
-    it('should not render an alert with text Welcome to the Client List when displayAlert from hook is false', () => {
+    it('should not render an alert with text New client has been added when displayAlert from hook is false', () => {
       useAlert.mockReturnValue(false);
       renderComponent();
       expect(screen.queryByText('New client has been added')).not.toBeInTheDocument();
