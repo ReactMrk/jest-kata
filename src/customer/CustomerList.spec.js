cf5bec1bd74f2076081c14a0d4e1fd90bb4d625e
Biarnés, Adrià <adria.biarnes@sky.uk>
2025-08-21 15:40:55 +0100

    Added comments and hints to the first exercice.

--- src/customer/CustomerList.spec.js
+++ src/customer/CustomerList.spec.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import { useContext } from 'react';
+import useAlert from './hooks/useAlert';
+
+// Replaces useContext with a Jest mock function for controlled testing
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useContext: jest.fn()
+}));
+
+// Hint 1: Mock useAlert using jest.mock(.....), so we can control its return value in tests
+
 
 const customers = [
   {
@@ -8,10 +19,26 @@ const customers = [
     address: 'Watermark'
   }
 ]
+const setHomeAlertText = jest.fn();
+// Hint 2: create another mock function, for setHomeAlertVisible
 
 describe('CustomerList', () => {
+  beforeEach(() => {
+    // Mocks the return value of useContext
+    useContext.mockReturnValue({ customers });
+
+    // TODO: Mock the return value of useAlert
+  });
+
+  // Clears mock call/instance history after every test, to avoid tests affecting each-other
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   describe('Client added alert', () => {
     it.todo('should render an alert with text Welcome to the Client List when displayAlert from hook is true');
+    // 1. Render the component
+    // 2. Check that the hooks have been called
+    // 3. Check that the alert is displayed (hint: check for the text in the alert)
 
     it.todo('should not render an alert with text Welcome to the Client List when displayAlert from hook is false');
   });
